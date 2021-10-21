41dc014e0262b6f863fc5d9a6f7748141d3fadb4
robertocannella <robertocannella@gmail.com>
2021-10-21 13:51:04 -0400

    Added welcome page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,21 +2,27 @@ import { ProjectsComponent } from './projects/projects.component';
 import { JournalComponent } from './journal/journal.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AlgorithmsComponent } from './algorithms/algorithms.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 
 const routes: Routes = [
-       {
-        path: '',
-        component: ProjectsComponent
-      },
-      {
-        path: 'journal',
-        component: JournalComponent
-        },
-        {
-        path: 'projects',
-        component: ProjectsComponent
-      },
+  {
+    path: '',
+    component: WelcomeComponent
+  },
+  {
+    path: 'journal',
+    component: JournalComponent
+  },
+  {
+    path: 'algorithms',
+    component: AlgorithmsComponent
+  },
+  {
+    path: 'projects',
+    component: ProjectsComponent
+  },
 
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { TopicsComponent } from './journal/topics/topics.component';
 import { NewEntryComponent } from './new-entry/new-entry.component';
 import { NewPostFormComponent } from './new-post-form/new-post-form.component';
 import { AlgorithmsComponent } from './algorithms/algorithms.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 @NgModule({
   declarations: [
@@ -23,7 +24,8 @@ import { AlgorithmsComponent } from './algorithms/algorithms.component';
     TopicsComponent,
     NewEntryComponent,
     NewPostFormComponent,
-    AlgorithmsComponent
+    AlgorithmsComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
