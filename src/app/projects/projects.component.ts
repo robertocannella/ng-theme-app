ca980b66752b60b9dec81b507d54f4b4ee0326a0
robertocannella <robertocannella@gmail.com>
2021-11-30 14:20:45 -0500

    Cleaned up links

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./projects.component.sass']
 })
 export class ProjectsComponent implements OnInit {
-
+  public innerWidth: number = 0;
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.innerWidth = window.innerWidth * .9;
   }
-
 }
