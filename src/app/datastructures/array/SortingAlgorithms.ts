5f5230d1d2e756c4bf9edfb8b267cba35a7ea010
robertocannella <robertocannella@gmail.com>
2022-02-03 21:11:06 -0500

    added mergeSort to array component

--- src/app/datastructures/array/SortingAlgorithms.ts
+++ src/app/datastructures/array/SortingAlgorithms.ts
@@ -1,6 +1,34 @@
 
 export class SortingAlgorithms {
+    mergeSort(array: number[]) {
+        this._mergeSort(array, 0, array.length - 1);
 
+    }
+    _mergeSort(array: number[], lo: number, hi: number) {
+        if (hi <= lo) return;
+
+        let mid: number = lo + Math.floor((hi - lo) / 2);
+        this._mergeSort(array, lo, mid);
+        this._mergeSort(array, mid + 1, hi);
+        this._merge(array, lo, mid, hi);
+
+    }
+    _merge(array: number[], lo: number, mid: number, hi: number) {
+        let aux: number[] = [];
+        let i: number = lo;
+        let j: number = mid + 1;
+
+        for (let k = lo; k <= hi; k++)
+            aux[k] = array[k];
+
+        for (let k = lo; k <= hi; k++) {
+            if (i > mid) array[k] = aux[j++];
+            else if (j > hi) array[k] = aux[i++];
+            else if (aux[j] < aux[i]) array[k] = aux[j++];
+            else array[k] = aux[i++];
+        }
+
+    }
     bubbleSort(array: number[]) {
         let isSorted;
 
--- src/app/datastructures/array/array.component.ts
+++ src/app/datastructures/array/array.component.ts
@@ -24,6 +24,7 @@ export class ArrayComponent implements OnInit {
   private height = 150;
   private vbWidth = 400;
   private vbHeight = 150;
+  animationColor = 'blue';
 
   private y = d3.scaleLinear().range([this.vbHeight, 0])
     .domain([0, Math.max(...this.ce) + 3]);
@@ -36,8 +37,8 @@ export class ArrayComponent implements OnInit {
   panScale: number = 1;
   _showIndex: boolean = true;
   _buttons: boolean = true;
-
-
+  allPromises: any = [];
+  _mergeCount: number = 0;
 
   constructor() { }
 
@@ -62,7 +63,7 @@ export class ArrayComponent implements OnInit {
         .on('end', (event: any) => {
           //console.log('zoom.end', event.transform, d3.zoomIdentity)
           if (event.transform !== d3.zoomIdentity) {
-            console.log(event.transform)
+            //console.log(event.transform)
             // ALLOWS TO RESET ZOOM PROGRAMATICALLY
             // remove anonymous function
             //d3.select(this).transition().delay(500).call(event.target.transform, d3.zoomIdentity)
@@ -70,22 +71,24 @@ export class ArrayComponent implements OnInit {
             this.panX = vectorPan.x;
             this.panY = vectorPan.y;
             this.panScale = vectorPan.k;
-            console.log(this.panX, this.panY, this.panScale)
+            //console.log(this.panX, this.panY, this.panScale)
 
           }
         })
       )
-    this.update();
+    this.update(this.ce);
 
   }
-  update() {
+  update(arr: number[]) {
+
+
 
     let xScale = d3.scaleLinear()
-      .domain([0, 50])
+      .domain([0, 50])  // default to 50
       .range([-100, 550])
 
     let groups = this.svg.select('svg#array-elements').selectAll('g')
-      .data(this.ce, (data: any, index: any, nodes: any) => data)
+      .data(arr, (data: any, index: any, nodes: any) => data)
       .enter()
       .append('g')
       .attr('id', (d: any, i: any) => 'group' + i + '-' + d)
@@ -99,7 +102,7 @@ export class ArrayComponent implements OnInit {
 
     groups
       .append('rect')
-      .attr('width', 10)
+      .attr('width', 10)  // default to 10
       .attr('height', 75)
       .classed('element', true)
       .attr('x', (d: any, i: any) => xScale(i.toString()))
@@ -107,10 +110,10 @@ export class ArrayComponent implements OnInit {
       .attr('fill', 'white')
       .attr('stroke-width', 1)
       .attr('stroke', 'black')
-      .attr('fill', 'purple')
+      .attr('fill', this.animationColor)
       .attr('fill-opacity', (d: any) => d / 150 + .05)
       .attr('class', 'element')
-      .attr('id', (d: any) => 'rect' + d)
+      .attr('id', (d: any, i: any) => 'rect' + d)
       .attr('transform', `translate(${this.panX},${this.panY}) scale(${this.panScale}, ${this.panScale})`)
 
     // groups
@@ -119,7 +122,7 @@ export class ArrayComponent implements OnInit {
     //   .attr('alignment-baseline', 'central')
     //   .attr('text-anchor', 'middle')
     //   .attr('class', 'element-value')
-    //   .attr('id', (d: any) => 'text' + d)
+    //   .attr('id', (d: any, i: any) => 'text' + d)
     //   .attr('x', (d: any, i: any) => xScale(i.toString()) + 25)
     //   .attr('y', 75)
     //   .attr('transform', `translate(${this.panX},${this.panY}) scale(${this.panScale}, ${this.panScale})`)
@@ -194,7 +197,7 @@ export class ArrayComponent implements OnInit {
     }
   }
   addRandomElement() {
-    if (this.ce.length > 50)
+    if (this.ce.length > 98)
       return;
 
     let value = this.randomIntFromInterval(0, 99)
@@ -203,7 +206,7 @@ export class ArrayComponent implements OnInit {
       value = this.randomIntFromInterval(1, 99)
 
     this.ce.push(value)
-    this.update();
+    this.update(this.ce);
   }
 
   async shuffle() {
@@ -212,7 +215,7 @@ export class ArrayComponent implements OnInit {
     for (var i = this.ce.length - 1; i > 0; i--) {
       var j = Math.floor(Math.random() * (i + 1));
 
-      await this.swapAnimation(this.ce[i], i, this.ce[j], j, 100)
+      await this.swapAnimation(this.ce[i], i, this.ce[j], j, 100, 0)
 
       var temp = this.ce[i];
       this.ce[i] = this.ce[j];
@@ -221,7 +224,118 @@ export class ArrayComponent implements OnInit {
     this.toggleButtons();
 
   }
+
+
+  async mergeSort() {
+    this.allPromises = [];
+    console.log(`this.ce = [${this.ce}]`)
+    this._mergeSort(this.ce, 0, this.ce.length - 1)
+    let count = 0;
+
+    //this.update();
+    console.log(this.allPromises)
+    for (let promise of this.allPromises) {
+      //console.log('count:', count++);
+      await this.replaceAnimation(promise.array1, promise.array2, promise.mainArray);
+    }
+
+
+  }
+  async _mergeSort(array: number[], lo: number, hi: number) {
+
+    if (hi <= lo) return
+
+    let mid: number = lo + Math.floor((hi - lo) / 2);
+    this._mergeSort(array, lo, mid);
+    this._mergeSort(array, mid + 1, hi);
+    await this._merge(array, lo, mid, hi);
+  }
+
+  async _merge(array: number[], lo: number, mid: number, hi: number) {
+
+    let aux: number[] = [];
+    let i: number = lo;
+    let j: number = mid + 1;
+
+
+    for (let k = lo; k <= hi; k++)
+      aux[k] = array[k];
+
+    console.log('=== section ===')
+
+
+    for (let m = lo; m <= hi; m++) {
+      // this.allPromises.push([...array])
+      //console.log('M:', m, 'I:', i, 'J', j)
+      if (i > mid) {
+
+        console.log(`Left half exhausted (${aux[i]}) (i: ${i}), take ${aux[j]} from RIGHT, set array[${m}] to index j: ${j}`)
+        console.log(`\ti: ${i}, j: ${j}, m: ${m}, lo: ${lo}, mid: ${mid}, hi: ${hi}, \n\t aux: ${aux}, array: ${array}`)
+        console.log(`\tAUX[j]: ${aux[j]}, AUX[i]: ${aux[i]}, ARRAY[m]: ${array[m]}`)
+        this.allPromises.push({ array1: array[m], index1: m, array2: aux[j], index2: j, duration: 400, delay: 500, mainArray: [...array] });
+
+        console.log('count: ', this._mergeCount++);
+        array[m] = aux[j];
+
+        j++;
+
+
+      }
+      else if (j > hi) {
+        console.log(`Right half exhausted (${aux[j]}) (j: ${j}), take ${aux[i]} from LEFT, set array [${m}] to index i ${i} `)
+        console.log(`\ti: ${i}, j: ${j}, m: ${m}, lo: ${lo}, mid: ${mid}, hi: ${hi}, \n\t aux: ${aux}, array: ${array}`)
+        console.log(`\tAUX[j]: ${aux[j]}, AUX[i]: ${aux[i]}, ARRAY[m]: ${array[m]}`)
+        this.allPromises.push({ array1: array[m], index1: m, array2: aux[i], index2: i, duration: 400, delay: 500, mainArray: [...array] });
+        console.log('count: ', this._mergeCount++);
+        array[m] = aux[i]
+
+        i++;
+
+      }
+      else if (aux[j] < aux[i]) {
+        console.log(`Current key (${aux[j]}) on right is LESS than current key (${aux[i]}) on left, take from the RIGHT set array[${m}] to aux index j: ${j}`)
+        console.log(`\ti: ${i}, j: ${j}, m: ${m}, lo: ${lo}, mid: ${mid}, hi: ${hi}, \n\t aux: ${aux}, array: ${array}`)
+        console.log(`\tAUX[j]: ${aux[j]}, AUX[i]: ${aux[i]}, ARRAY[m]: ${array[m]}`)
+
+        this.allPromises.push({ array1: array[m], index1: m, array2: aux[j], index2: j, duration: 400, delay: 500, mainArray: [...array] });
+        console.log('count: ', this._mergeCount++);
+        array[m] = aux[j];
+
+        j++;
+
+      }
+      else {
+        console.log(`Current key  (${aux[j]}) on right is GREATER than or EQUAL to current key (${aux[i]}) on left, take from the LEFT, set array[${m}] to aux index i: ${i}]`)
+        console.log(`\ti: ${i}, j: ${j}, m: ${m}, lo: ${lo}, mid: ${mid}, hi: ${hi}, \n\t aux: ${aux}, array: ${array}`)
+        console.log(`\tAUX[j]: ${aux[j]}, AUX[i]: ${aux[i]}, ARRAY[m]: ${array[m]}`)
+
+        this.allPromises.push({ array1: array[m], index1: m, array2: aux[i], index2: i, duration: 400, delay: 500, mainArray: [...array] });
+        console.log('count: ', this._mergeCount++);
+        array[m] = aux[i];
+
+        i++;
+      }
+
+    }
+    this.allPromises.push({ array1: 0, index1: 0, array2: 0, index2: 0, duration: 400, delay: 500, mainArray: [...this.ce] });
+
+    console.log(`this.ce = [${this.ce}]`)
+
+  }
   async shellSort() {
+    let window = d3.select('svg#array-elements')
+    window.select('.sort-detail').remove();
+    window
+      .append('text')
+      .text('1')
+      .attr('alignment-baseline', 'central')
+      .attr('text-anchor', 'middle')
+      .attr('class', 'sort-detail')
+      .attr('x', -80)
+      .attr('y', 25)
+      .attr('transform', `translate(${this.panX},${this.panY}) scale(${this.panScale}, ${this.panScale})`)
+
+
     this.toggleButtons();
     let n = this.ce.length;
     let h = 1;
@@ -229,19 +343,24 @@ export class ArrayComponent implements OnInit {
       h = Math.floor(3 * h + 1); // 1, 4,13,40,121,363,1093
 
     while (h >= 1) { // h-sort the array
+      window.select('.sort-detail')
+        .text(() => `h =  ${h}`)
       for (let i = h; i < n; i++) {
-        console.log(h);
         for (let j = i; j >= h && this.less(this.ce[j], this.ce[j - h]); j -= h) {
 
-          await this.swapAnimation(this.ce[j], j, this.ce[j - h], j - h, 100);
+          await this.swapAnimation(this.ce[j], j, this.ce[j - h], j - h, 100, 0)
           this.exchange(this.ce, j, j - h);
+
         }
       }
       h = Math.floor(h / 3);
     }
-    console.log(this.ce)
+    window.select('.sort-detail')
+      .transition()
+      .duration(299)
+      .remove();
     this.toggleButtons();
-    this.update();
+    this.update(this.ce);
 
   }
   async insertionSort() {
@@ -251,12 +370,12 @@ export class ArrayComponent implements OnInit {
     for (let i = 0; i < n; i++) {
       for (let j = i; j > 0 && this.less(this.ce[j], this.ce[j - 1]); j--) {
 
-        await this.swapAnimation(this.ce[j], j, this.ce[j - 1], j - 1, 100)
+        await this.swapAnimation(this.ce[j], j, this.ce[j - 1], j - 1, 100, 0)
         this.exchange(this.ce, j, j - 1);
       }
     }
     this.toggleButtons();
-    this.update();
+    this.update(this.ce);
 
   }
   async selectionSort() { // Sort a[] into increasing order
@@ -269,12 +388,12 @@ export class ArrayComponent implements OnInit {
         if (this.less(this.ce[j], this.ce[min])) min = j;
 
 
-      await this.swapAnimation(this.ce[i], i, this.ce[min], min, 200)
+      await this.swapAnimation(this.ce[i], i, this.ce[min], min, 200, 0);
       this.exchange(this.ce, i, min);
 
     }
     this.toggleButtons();
-    this.update();
+    this.update(this.ce);
   }
 
   async bubbleSort() {
@@ -289,26 +408,42 @@ export class ArrayComponent implements OnInit {
           this.ce[j] = this.ce[j + 1]
           this.ce[j + 1] = temp
           isSorted = false;
-          await this.swapAnimation(this.ce[j], j, this.ce[j + 1], j + 1, 100)
+          await this.swapAnimation(this.ce[j], j, this.ce[j + 1], j + 1, 100, 0)
         }
 
       }
     } while (!isSorted)
 
     this.toggleButtons()
-    this.update();
+    this.update(this.ce);
   }
-  swapAnimation(d: any, i: any, d1: any, i1: any, durationTime: number) {
+  async replaceAnimation(array1: any, array2: any, mainArray: number[]) {
+    let groups = this.svg.select('svg#array-elements').selectAll('g')
 
-    // let textSel = `#text${d}`;
-    // let textSel1 = `#text${d1}`;
+    return Promise.all([
+      groups.remove(),
+
+      this.update(mainArray),
+
+      groups
+        .transition()
+        .duration(25)
+        .end()
+    ])
+
+  }
+  swapAnimation(d: any, i: any, d1: any, i1: any, durationTime: number, delayTime: number) {
+
+
+    let textSel = `#text${d}`;
+    let textSel1 = `#text${d1}`;
     let rectSel = `#rect${d}`;
     let rectSel1 = `#rect${d1}`;
     // console.log(textSel, textSel1, rectSel, rectSel1)
 
 
-    // let textSelX = d3.select(textSel).attr('x');
-    // let textSel1X = d3.select(textSel1).attr('x');
+    let textSelX = d3.select(textSel).attr('x');
+    let textSel1X = d3.select(textSel1).attr('x');
     let rectSelX = d3.select(rectSel).attr('x');
     let rectSel1X = d3.select(rectSel1).attr('x');
     if (rectSel1 === rectSel)     // added this for section sort error on duplicate tags
@@ -316,45 +451,55 @@ export class ArrayComponent implements OnInit {
 
     return Promise.all([
 
-      // d3.select(textSel)
-      //   .transition()
-      //   .duration(durationTime)
-      //   .attr('x', textSel1X)
-      //   .end(),
 
-      // d3.select(textSel1)
-      //   .transition()
-      //   .duration(durationTime)
-      //   .attr('x', textSelX)
-      //   .end(),
+      d3.select(textSel)
+        .transition()
+        .duration(durationTime)
+        .delay(delayTime)
+        .attr('x', textSel1X)
+        .end().catch(error => console.log(error)),
+
+      d3.select(textSel1)
+        .transition()
+        .duration(durationTime)
+        .delay(delayTime)
+        .attr('x', textSelX)
+        .end().catch(error => console.log(error)),
 
       d3.select(rectSel)
         .transition()
         .duration(durationTime)
-        .attr('x', rectSel1X)
+        .delay(delayTime)
+        .attr('x', () => {
+          console.log('3rd')
+          return rectSel1X
+        })
         // .attr('fill', 'grey')
         // .transition()
         // .duration(100)
-        // .attr('fill', 'purple')
-        .end(),
+        // .attr('fill', 'orange')
+        .end().catch(error => console.log(error)),
 
       d3.select(rectSel1)
         .transition()
         .duration(durationTime)
+        .delay(delayTime)
         .attr('x', rectSelX)
         // .attr('fill', 'grey')
         // .transition()
         // .duration(100)
-        // .attr('fill', 'purple')
-        .end()
+        // .attr('fill', 'orange')
+        .end().catch(error => console.log(error))
 
-    ])
+
+    ]).catch(error => {
+      console.log(error.message)
+    })
 
   }
   // --- UTILTIY FUNCTIONS ---- //
   playAnimation() {
     this.toggleButtons();
-
 
     this.toggleButtons();
   }
