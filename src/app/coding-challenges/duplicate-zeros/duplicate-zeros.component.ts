6ba57b7e10eba36febafabf3e6cfe5aaa17f3c75
robertocannella <robertocannella@gmail.com>
2022-02-20 13:30:14 -0500

    improved interactivity duplicate-zeros

--- src/app/coding-challenges/duplicate-zeros/duplicate-zeros.component.ts
+++ src/app/coding-challenges/duplicate-zeros/duplicate-zeros.component.ts
@@ -8,12 +8,12 @@ import { BreakpointObserver, LayoutModule, BreakpointState, Breakpoints } from '
   styleUrls: ['./duplicate-zeros.component.sass', '../coding-challenges.component.sass']
 })
 export class DuplicateZerosComponent implements OnInit, OnDestroy {
-  datasetStage1 = [4, 3, 3, 7, 0, 3, 0, 3]
-  datasetStage2 = [...this.datasetStage1];
 
+  // Page Setup
+  isHandheld: boolean = false;
+
+  // SVG Constansts
   defaultDuration = 500;
-  _buttons = false;
-  _buttonsStage2 = false;
   width = 600;
   height = 200;
   vbWidth = 325;
@@ -21,31 +21,41 @@ export class DuplicateZerosComponent implements OnInit, OnDestroy {
   xmlns = 'http://www.w3.org/2000/svg';
   svgStage1 = 'coding-outlet-1';
   svgStage2 = 'coding-outlet-2'
+
+
+  // Button Toggles/Contols
+  _buttons = false;
+  _buttonsStage2 = false;
+  _topButtons = false;
   isPlayingAnimation = false;
   isPlayingAnimationStage2 = false;
   isPlayingRandomSeq = false;
+
+  // For Displaying content in DOM 
   currentI = 0
   currentJ = 0
   currentIStage2 = 0
   currentJStage2 = 0
   totalZerosStage1 = 0
   totalZerosStage2 = 0
+  // Not used - to be removed
   Stage1i = 0
 
 
-  isHandheld: boolean = false;
+  // Starter datasets for each stage
+  datasetStage1 = [4, 3, 3, 7, 0, 3, 0, 3]
+  datasetStage2 = [...this.datasetStage1];
+
 
   constructor(public breakpointObserver: BreakpointObserver) { }
 
   /// Angular Methods
   ngOnInit(): void {
-
     this.breakpointObserver.observe([
       Breakpoints.XSmall
     ]).subscribe((state: BreakpointState) => {
       this.isHandheld = (state.matches)
     });
-
     this.buildSVG();
     this.updateStage1();
     this.updateStage2();
@@ -74,6 +84,7 @@ export class DuplicateZerosComponent implements OnInit, OnDestroy {
     this.isPlayingRandomSeq = true
     this._buttons = true
 
+
     this.datasetStage2 = [...this.datasetStage1]
     this.updateStage2();
     this.updateStage1();
@@ -90,12 +101,15 @@ export class DuplicateZerosComponent implements OnInit, OnDestroy {
     this.isPlayingAnimation = false
     this.isPlayingAnimationStage2 = false
     this.isPlayingRandomSeq = false
+    this._topButtons = false
+    this._buttons = false
   }
-
-  async randomAll(sizeEach: number = this.getRandomInt(9, 9)) {
+  async randomAll(sizeEach: number = this.getRandomInt(12, 12)) {
 
     this.isPlayingRandomSeq = true
     this.isPlayingAnimation = true
+    this.isPlayingAnimationStage2 = true;
+
     this._buttons = true
     while (this.isPlayingAnimation) {
 
@@ -104,6 +118,8 @@ export class DuplicateZerosComponent implements OnInit, OnDestroy {
       for (let j = 0; j < sizeEach; j++) {
         this.datasetStage1.push(this.getRandomInt(0, 9))
       }
+      if (!this.datasetStage1.includes(0))
+        continue;
       this.datasetStage2 = [...this.datasetStage1]
 
       this.updateStage1();
@@ -119,8 +135,13 @@ export class DuplicateZerosComponent implements OnInit, OnDestroy {
   stopRandomAll() {
     this.isPlayingRandomSeq = false
     this.isPlayingAnimation = false
+    this.isPlayingAnimationStage2 = false
     this.resetAll();
   }
+
+  //********************** DOM BUTTON LOGIC
+  //
+  //
   // Stage 1 Buttons
   async test(sizeEach: number = this.getRandomInt(9, 9)) {
     this.isPlayingAnimation = true
@@ -171,12 +192,15 @@ export class DuplicateZerosComponent implements OnInit, OnDestroy {
       for (let j = 0; j < sizeEach; j++) {
         this.datasetStage2.push(this.getRandomInt(0, 9))
       }
+
       this.updateStage2();
       await this._animateStage2();
     }
   }
 
-  //// BEGIN STAGE 1 ANIMATIONS METHODS
+  //********************** BEGIN STAGE 1 METHODS *************************** //
+  //
+  //
   // STAGE 1 Update
   updateStage1() {
     d3.select('#coding-outlet-1').selectAll('*').remove()
@@ -214,67 +238,78 @@ export class DuplicateZerosComponent implements OnInit, OnDestroy {
             })
         })
   }
-  /// STAGE 1 Animation
-  async _animateStage1() {
-
-    this.isPlayingAnimation = true;
-    let n = this.datasetStage1.length - 1;
-    this.totalZerosStage1 = 0;
-    let coords: any[] = [];
-
-    for (let i = 0; i <= n; i++) {
-      let currRect = d3.select(`#rect${i}-${this.datasetStage1[i]}`);
-      let currText = d3.select(`#text${i}-${this.datasetStage1[i]}`);
-      coords.push({ rectX: currRect.attr('x'), textX: currText.attr('x') });
-    }
-
-    for (let i = 0; i <= n; i++) { // begin animation loop 
-
-      let curr = `#rect${i}-${this.datasetStage1[i]}`;
-      this.currentI = i;
-      this.currentJ = 0;
-      let remaingSpace = n - i;
-      let currRect = d3.select(curr)
-      await currRect   // set tile colors here
-        .transition()
-        .duration(200)
-        .attr('fill', (_data: any, index: any, nodes: any) => {
-          if (this.datasetStage1[i] === 0) { // initial loop
-            if (this.totalZerosStage1 >= remaingSpace) {
-              return '#79d14d'; // green
-            }
-            d3.select(nodes[index]).attr('isDuplicate', true);
-            this.totalZerosStage1++;
-            return 'cornflowerblue';
-          }
-          else {
-            if (i > n - this.totalZerosStage1) return '#fcba03' // yellow
-            return '#79d14d'  // green
-          }
-        }).on('interrupt', () => { i = this.datasetStage1.length + 1; return; })
-        .end().catch((e) => { i = this.datasetStage1.length + 1; this.resetAll(); console.log('promise interrupted ANIME A'), e })
-
-
-      if (this.datasetStage1[i] === 0 && currRect.attr('isDuplicate')) // trigger second loop Loop
-        await this.checkZeros(i, coords).catch((e) => {
-          console.log('Promise Interrupted in _animateStage1(): ', e); i = this.datasetStage1.length + 1; this.resetAll();;
-        })
-    }
-  }
+  // STAGE 1 Animation
   async animateStage1() {
+    this._topButtons = true;
     await this._animateStage1().catch(e => console.log(e));
+
     this.Stage1i = 0
+
     //this.updateStage1();
+  }
+  async _animateStage1() {
+    return new Promise(async (resolve) => {
+
+
+      this.isPlayingAnimation = true;
+      let n = this.datasetStage1.length - 1;
+      this.totalZerosStage1 = 0;
+      let coords: any[] = [];
+
+      for (let i = 0; i <= n; i++) {
+        let currRect = d3.select(`#rect${i}-${this.datasetStage1[i]}`);
+        let currText = d3.select(`#text${i}-${this.datasetStage1[i]}`);
+        coords.push({ rectX: currRect.attr('x'), textX: currText.attr('x') });
+      }
+
+      for (let i = 0; i <= n; i++) { // begin animation loop 
+
+        let curr = `#rect${i}-${this.datasetStage1[i]}`;
+        this.currentI = i;
+        this.currentJ = 0;
+        let remaingSpace = n - i;
+        let currRect = d3.select(curr)
+        await currRect   // set tile colors here
+          .transition()
+          .duration(200)
+          .attr('fill', (_data: any, index: any, nodes: any) => {
+            if (this.datasetStage1[i] === 0) { // initial loop
+              if (this.totalZerosStage1 >= remaingSpace) {
+                return '#79d14d'; // green
+              }
+              d3.select(nodes[index]).attr('isDuplicate', true);
+              this.totalZerosStage1++;
+              return 'cornflowerblue';
+            }
+            else {
+              if (i > n - this.totalZerosStage1) return '#fcba03' // yellow
+              return '#79d14d'  // green
+            }
+          }).on('interrupt', () => { i = this.datasetStage1.length + 1; return; })
+          .end().catch((e) => { i = this.datasetStage1.length + 1; this.stopAnimation(); })
+
+
+        if (this.datasetStage1[i] === 0 && currRect.attr('isDuplicate')) // trigger second loop Loop
+          await this.checkZeros(i, coords).catch((e) => {
+            console.log('Promise Interrupted in _animateStage1(): ', e); i = this.datasetStage1.length + 1; this.stopAnimation();;
+          })
+
+      }
+      setTimeout(resolve, 200)
+    })
+
   }
   /// STAGE 1 Helper Methods
   stopAnimation() {
     this.totalZerosStage1 = 0;
+    if (!this.isPlayingAnimationStage2)
+      this._topButtons = false;
 
     d3.select(`#${this.svgStage1}`).selectAll('*').interrupt()
     d3.select(`#${this.svgStage1}`).selectAll('g').remove()
     this.updateStage1();
+
     this.isPlayingAnimation = false;
-    this._buttons = false;
   }
   async checkZeros(index: number, coords: any[]) {
     return new Promise(async (resolve, recject) => {
@@ -284,7 +319,7 @@ export class DuplicateZerosComponent implements OnInit, OnDestroy {
         })
           .catch(e => { recject(e); })
       }
-      setTimeout(resolve, 0)
+      setTimeout(resolve, 200)
     })
   }
   async shiftElements(coords: any[], index: number) {
@@ -363,7 +398,10 @@ export class DuplicateZerosComponent implements OnInit, OnDestroy {
     ]).catch((e) => { console.log('insertZeros promise') })
 
   }
-  //// BEGIN STAGE 2 ANIMATION METHODS
+
+  //********************** BEGIN STAGE 2 METHODS *************************** //
+  //
+  //
   // STAGE 2 Update
   updateStage2() {
     d3.select('#coding-outlet-2').selectAll('*').remove()
@@ -402,27 +440,31 @@ export class DuplicateZerosComponent implements OnInit, OnDestroy {
             })
         })
   }
-  /// STAGE 2 Stop Animation
+
+  // STAGE 2 Animations
+  async animateStage2() {
+    this._topButtons = true;
+    await this._animateStage2();
+
+  }
+  async _animateStage2() {
+
+    this.isPlayingAnimationStage2 = true;
+    await this.firstPassStage2().catch(_e => { console.log('Promise Interrupted') })
+    await this.secondPassStage2().catch(_e => { console.log('Promise Interrupted') })
+    await this.timeout(2000) // time between each iteration
+  }
+
+  // STAGE 2 Helper Methods
   async stopAnimationStage2() {
+    if (!this.isPlayingAnimation)
+      this._topButtons = false;
     this.totalZerosStage2 = 0;
     d3.select(`#${this.svgStage2}`).selectAll('*').interrupt()
     this.updateStage2();
     this.isPlayingAnimationStage2 = false;
-  }
-  async animateStage2() {
-    await this._animateStage2();
-    this._buttonsStage2 = false
 
   }
-  /// STAGE 2 Animation
-  async _animateStage2() {
-    this._buttonsStage2 = true;
-    this.isPlayingAnimationStage2 = true;
-    await this.firstPassStage2().catch(_e => { console.log('Promise Interrupted') })
-    await this.secondPassStage2().catch(_e => { console.log('Promise Interrupted') })
-    await this.timeout(500) // time between each iteration
-  }
-  /// STAGE 2 Helper Methods
   async firstPassStage2() {
     let n = this.datasetStage2.length - 1;
     this.totalZerosStage2 = 0;
@@ -483,12 +525,6 @@ export class DuplicateZerosComponent implements OnInit, OnDestroy {
       }
     }).catch(e => console.log(e))
   }
-  updateAwait() {
-    return new Promise((resolve) => {
-      this.updateStage2();
-      resolve(true)
-    })
-  }
   insertAnimation(index: number, coords: any[]) {
     return new Promise(async (resolve, _reject) => {
       let lastRect = d3.select(`#rect${index}-${this.datasetStage2[index]}-stage2`)
@@ -535,7 +571,7 @@ export class DuplicateZerosComponent implements OnInit, OnDestroy {
         .attr('stroke', 1)
         .attr('stroke-width', 1)
         .attr('opacity', 0)
-        .transition().duration(200).attr('opacity', 1)
+        .transition().duration(300).attr('opacity', 1)
 
       d3.select(`#${this.svgStage2}`).append('g')
         .append('text')
@@ -552,7 +588,9 @@ export class DuplicateZerosComponent implements OnInit, OnDestroy {
       resolve(true)
     })
   }
-  /// Utility Functions
+  //********************** BEGIN COMMON UTILITY METHODS *************************** //
+  //
+  //
   timeout(ms: number) {
     return new Promise(resolve => setTimeout(resolve, ms));
   }
